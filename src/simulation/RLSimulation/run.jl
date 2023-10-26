f585debc6ce52a68b2b8db6b89955936c7eefe26
Jun He <he184@outlook.com>
2023-10-26 11:40:36 -0400

    basic workflow

--- src/simulation/RLSimulation/run.jl
+++ src/simulation/RLSimulation/run.jl
@@ -15,18 +15,38 @@ function simulate!(
             # proceed the time at the beginning
             _proceed_time!(instance.clock)
 
-            # market remove old orders
+            # market remove old orders, some agents receive reward
 
+            # learning 
+
+            # select agents
+            selected_agents = []
             while _get_time_counter_from_continuous_time(
                     instance.clock, arrival_time) < instance.clock.time_counter
                 # get next sample
                 arrival_time += instance.market.arrival === nothing ? 1.0 : rand(instance.market.arrival)
                 # choose an agent
                 selected_agent = _random_enter_market(instance.agents, instance.clock.time_counter)
-                # let agent submit the order 
+                push!(selected_agents, selected_agent)
             end
 
-            # market match and process order 
+            # all agents not selected prosume 
+
+            # generate agent decision 
+
+            # market receive and sort orders 
+
+            # market match and process order, some agents receive reward
+
+            # reset flow to prevent carry over
+            for line in instance.network.lines
+                _reset_line_flow!(line)
+            end
+
+            # finally snapshot all traders' current price & storage
+            for agent in instance.agents    
+                _append_current_trader_price!(agent.trader)
+            end
         end
     end
 end
