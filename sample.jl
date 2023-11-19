4e7ad02d41504d805a6247018e528e5b98d277b8
Jun He <he184@outlook.com>
2023-11-19 13:20:48 -0500

    Update sample.jl

--- sample.jl
+++ sample.jl
@@ -2,5 +2,10 @@ include("src/EnergyTrading.jl")
 using .EnergyTrading 
 
 fpath = "./sample.json"
-instance = EnergyTrading.read(fpath);
-EnergyTrading.simulate!(instance, EnergyTrading.RLSimulation())
+instance = EnergyTrading.read(fpath)
+EnergyTrading.load_models!(instance, dir = "./saved_states", ignore_nonexisiting=true)
+EnergyTrading.simulate!(instance, EnergyTrading.RLSimulation(evaluate=false))
+EnergyTrading.save_models(instance, dir = "./saved_states")
+
+solution = EnergyTrading.solution(instance)
+EnergyTrading.write("output.json", solution)
