1ccc2f7daf9e5ae424ac5e02c00561d7b3c9820b
Jun He <he184@outlook.com>
2023-11-19 13:20:43 -0500

    lstm

--- src/trader/RLTrader/neural_network.jl
+++ src/trader/RLTrader/neural_network.jl
@@ -2,19 +2,21 @@ using Flux
 
 # store model and opt state in a struct 
 struct Network 
+    name::String
     model
     opt_state
 end
 
 # construct basic neural network
 function _construct_neural_network(
+    name::String,
     in_dim::Int, 
     out_dim::Int;
     activation = nothing
 )::Network
     # specify the model
     fc1 = Dense(in_dim => 64, relu)
-    fc2 = Dense(64 => 64, relu)
+    fc2 = LSTM(64 => 64)
     if activation === "tanh"
         out = Dense(64 => out_dim, tanh)
     elseif activation === "sigmoid"
@@ -26,5 +28,5 @@ function _construct_neural_network(
     model = Chain(fc1, fc2, out) |> gpu 
     opt_state = Flux.setup(Adam(), model)
 
-    return Network(model, opt_state)
+    return Network(name, model, opt_state)
 end
